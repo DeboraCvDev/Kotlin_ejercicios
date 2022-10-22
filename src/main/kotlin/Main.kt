b506c0127a0517ea49944d09abbde505814bab2f
DeboraCvDev <deboracornelio14@gmail.com>
2022-10-22 17:27:00 -0500

    ejercicio4 terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
+    println("Programa para calcular la suma de dos numeros enteros")
+    println("Ingrese primer numero:")
+    val primerNumero: Int = readLine()!!.toInt()
+    println("Ingrese segundo numero:")
+    val segundoNumero: Int = readLine()!!.toInt()
 
+    val suma: Int = calcularSuma(primerNumero = primerNumero, segundoNumero = segundoNumero)
+    imprimirlaSumaDePrimerNumeroYsegundoNumero(primerNumero = primerNumero, segundoNumero = segundoNumero, suma = suma)
+
+    //imprimirlaSumaDePrimerNumeroYsegundoNumero(primerNumero = primerNumero, segundoNumero = segundoNumero)
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,11 @@
+fun calcularSuma(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero + segundoNumero
+
+}
+
+//fun imprimirlaSumaDePrimerNumeroYsegundoNumero(primerNumero: Int,segundoNumero: Int) {
+//println("La suma de $primerNumero+$segundoNumero es:${calcularSuma(primerNumero = primerNumero, segundoNumero = segundoNumero)}")
+//}
+fun imprimirlaSumaDePrimerNumeroYsegundoNumero(primerNumero: Int, segundoNumero: Int, suma: Int) {
+    println("La suma de $primerNumero+$segundoNumero es:$suma")
+}
