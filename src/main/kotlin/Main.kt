618ef62bbf07c04d11c8bcbf7094e3167949c33e
DeboraCvDev <deboracornelio14@gmail.com>
2022-10-22 17:59:44 -0500

    ejercicio5  terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
-
+println("Programa que calcule el cuadrado de un numero")
+    println("Ingrese un numero ")
+    val numero:Int= readLine()!!.toInt()
+    val cuadrado:Int=calcularElCuadradoDeUnNumero(numero=numero)
+    imprimirelCuadradoDeUnNumero(numero=numero,cuadrado=cuadrado)
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,7 @@
+fun calcularElCuadradoDeUnNumero(numero:Int):Int{
+    return numero*numero
+
+}
+fun imprimirelCuadradoDeUnNumero(numero: Int,cuadrado:Int){
+    println("El cuadrado de $numero es:$cuadrado")
+}
