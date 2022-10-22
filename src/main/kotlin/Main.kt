bf7086c6173da876e4df2f45e8deb1164fe81c93
DeboraCvDev <deboracornelio14@gmail.com>
2022-10-22 15:47:19 -0500

    ejercicio2 terminado prueba

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
+    println("Calcular las suma de 2 numeros:")
 
+    println("INGRESE PRIMER NUMERO:")
+    val  primerNumero:Int= readLine()!!.toInt()
+
+    println("INFRESE SEGUNDO NUMERO:")
+    val segundoNumero:Int= readLine()!!.toInt()
+    val suma:Int
+    suma = calcularSumaDeDosNumeros(primerNumero = primerNumero, segundoNumero = segundoNumero)
+    imprimirSumaDeDosNumeros(primerNumero = primerNumero,segundoNumero=segundoNumero, suma = suma)
 }
---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,10 @@
+
+//ESTO ES UNA FUNCION Y LAS FUNCIONES RETORNAN ALGO
+fun calcularSumaDeDosNumeros(primerNumero:Int,segundoNumero:Int):Int{
+ return primerNumero+segundoNumero
+}
+
+//ESTE ES UN METODO Y LOS METODO NO RETORNAN UN RESULTADO SINO SOLO SE USAN PARA PINTAR UNA RESPUESTA
+fun imprimirSumaDeDosNumeros(primerNumero: Int,segundoNumero: Int,suma:Int){
+    println("La suma de $primerNumero y $segundoNumero es:->>>>>$suma")
+}
